688846f46211f3a7a3318d22b956a461c8484945
Abdullah Adeel <easyautosol@gmail.com>
2022-03-05 23:43:25 +0500

    ✅ added required `modules`

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return {
+      message: 'signup',
+    };
+  }
+
+  signin() {
+    return {
+      message: 'signin',
+    };
+  }
+}
